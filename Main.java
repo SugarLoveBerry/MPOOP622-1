7383d57af9e84bb6d295c1e456abf8a438f2ad2a
Axel-JovaniJova <>
2021-10-21 19:14:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+/**
+* @author Axel Ruiz
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    System.out.println("La suma es :"+suma(2,2));
+  }
+  /**
+  *@param a Primer entero de la ecuación
+  *@param b Segunfo entero de la suma
+  *@return  La suma de los entero a más b
+  */
+  public static int suma(int a,int b){
+  return a+b;                            
+  }
+}
